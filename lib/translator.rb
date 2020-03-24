913d962daa9e1b00ec4d39194a2ebbb74cfb9828
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-24 17:16:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def load_library(path)
     japanese_emoticon = value[1]
     english_emoticon = value[0]
     memo[:get_meaning][japanese_emoticon] = key
+    memo[:get_emoticon][english_emoticon] = key
     memo
   end
 end
